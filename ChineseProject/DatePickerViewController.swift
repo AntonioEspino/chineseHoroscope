4972a4dabd7bfd63c7d9c5f785f16e63a741a3fa
Antonio Espino Muñoz <antonioespinom@gmail.com>
2020-02-20 20:23:30 +0000

    funciona datepicker ^^

---
+++ ChineseProject/DatePickerViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DatePickerViewController.swift
+//  ChineseProject
+//
+//  Created by user164220 on 20/02/2020.
+//  Copyright © 2020 Antonio Espino. All rights reserved.
+//
+
+import UIKit
+
+import Foundation
+
+protocol DatePickerDelegate {
+    func datePickerValue (date: Date)
+}
+
+class DatePickerViewController: UIViewController  {
+    var delegate: DatePickerDelegate?
+    
+    @IBOutlet var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        delegate?.datePickerValue(date: datePicker.date)
+    }
+    
+    
+}
---
+++ ChineseProject/InicioViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ChineseProject
+//
+//  Created by user164220 on 20/02/2020.
+//  Copyright © 2020 Antonio Espino. All rights reserved.
+//
+
+import UIKit
+
+class InicioViewController: UIViewController{
+    
+    var datePicker = Date.init()
+    @IBAction func didTapInDiscoverYourSign(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func unwindToIntroduction(unwindSegue: UIStoryboardSegue) {
+        let control = DatePickerViewController()
+        control.delegate = self
+        
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDatePicker" {
+            let vc = segue.destination as! DatePickerViewController
+            vc.delegate = self
+        }
+    }
+    
+}
+extension InicioViewController: DatePickerDelegate {
+    func datePickerValue(date: Date) {
+        datePicker = date
+        performSegue(withIdentifier: <#T##String#>, sender: <#T##Any?#>)
+    }
+}
+
